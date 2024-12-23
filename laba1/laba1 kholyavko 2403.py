5d9450e8058183c9594baf71b5c91a705baed9c1
anyastar1 <miss.kholyavko2006@gmail.com>
2024-12-23 21:42:59 +0300

    Add files via upload

---
+++ laba1/laba1 kholyavko 2403.py
@@ -0,0 +1,15 @@
+#задача 1
+num= int(input("Введите число: "))
+x=1
+while x<=num:
+    print(x)
+    x+=1
+#задача 2
+num1= int(input("Введите число: "))
+num2= int(input("Введите число: "))
+if num1>num2:
+    print("Большее число:", num1)
+if num1<num2:
+    print("Большее число:", num2)
+if num1==num2:
+    print("Числа равны")
